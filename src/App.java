c453e34455c9aa2b4483689553694aeb8e18eb2a
oyeong011 <oyeong011@gmail.com>
2023-10-23 10:27:59 +0900

    10.23

---
+++ src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+       				//  각 문자는 상응하는 정수를 가짐
+                    char ch1 = 'A';
+                    char ch2 = 'B';
+                    char ch3 = 'a';
+                    char ch4 = 'a' + 1;
+                    char ch5 = '가';
+                    char ch6 = '가' + 1;
+                    char ch7 = '가' + 2;
+                    char ch8 = '가' + 3;
+                    char ch9 = '나';
+            
+                    int ch1Int = (int) ch1;
+                    int ch9Int = (int) ch9;
+                    int int_d1 = Character.getNumericValue(ch9Int);
+                }
+}
