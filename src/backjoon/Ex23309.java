be704d39478075b93111a5afac6a0336a09be867
oyeong011 <oyeong011@gmail.com>
2023-10-28 13:21:08 +0900

    23309번 배열로 구현했으나 시간초과

--- src/backjoon/Ex23309.java
+++ src/backjoon/Ex23309.java
@@ -9,26 +9,49 @@ import java.util.*;
 public class Ex23309 {
 
     private static class Station {
-        int absoluteNum;
-        Station next;
-        Station prev;
+        int[] nextNode;
+        int[] prevNode;
 
-        Station(Station prev, int absoluteNum, Station next) {
-            this.absoluteNum = absoluteNum;
-            this.next = next;
-            this.prev = prev;
+        Station() {
+            this.nextNode = new int[10000001];
+            this.prevNode = new int[10000001];
         }
 
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            Station cur = this;
-            do {
-                sb.append(cur.absoluteNum).append(" -> ");
-                cur = cur.next;
-            } while (cur != this);
-            if (sb.length() > 4) {
-                sb.setLength(sb.length() - 4);
+        public void add(int target, int uniqueNumber) {
+            if (target == -1) {
+                nextNode[uniqueNumber] = prevNode[uniqueNumber] = uniqueNumber;
+                return;
             }
+            // 신규 노드와 신규노드 오른쪽 노드와 연결
+            prevNode[nextNode[target]] = uniqueNumber;
+            nextNode[uniqueNumber] = nextNode[target];
+            // 신규 노드와 왼쪽노드와 연결
+            nextNode[target] = uniqueNumber;
+            prevNode[uniqueNumber] = target;
+        }
+
+        public void delete(int target) {
+            if (nextNode[target] == target) {
+                return;
+            }
+            // 오른쪽 노드랑 왼쪽노드 연결시키기
+            prevNode[nextNode[target]] = prevNode[target];
+            nextNode[prevNode[target]] = nextNode[target];
+
+            // 지울 노드 양쪽 연결요소 삭제
+            nextNode[target] = prevNode[target] = 0;
+
+        }
+
+        public String toString(int target) {
+            StringBuilder sb = new StringBuilder();
+            int searchNumber = target;
+            do {
+                sb.append(searchNumber + " -> ");
+                searchNumber = nextNode[searchNumber];
+            } while (searchNumber != target);
+            sb.setLength(sb.length() - 4);
+
             return sb.toString();
         }
     }
@@ -43,103 +66,59 @@ public class Ex23309 {
         st = new StringTokenizer(br.readLine(), " ");
         int uniqueNumber = Integer.parseInt(st.nextToken());
 
-        Station head = new Station(null, uniqueNumber, null);
-        Station station = head;
+        Station stationList = new Station();
+        int target = -1;
+        stationList.add(target, uniqueNumber);
+        target = uniqueNumber;
+
         for (int i = 0; i < stationCount - 1; i++) {
             uniqueNumber = Integer.parseInt(st.nextToken());
-            Station newStation = new Station(station, uniqueNumber, null);
-            station.next = newStation;
-            station = newStation;
+            stationList.add(target, uniqueNumber);
+            target = uniqueNumber;
         }
-        head.prev = station;
-        station.next = head;
 
-        int idx, newAbsoluteNum;
+        System.out.println(stationList.toString(2));
+
         StringBuilder sb = new StringBuilder();
-        Station newStation, removeStation, leftStation, rightStation;
         for (int i = 0; i < construction; i++) {
             st = new StringTokenizer(br.readLine(), " ");
             switch (st.nextToken()) {
                 case "BN":
-                    idx = Integer.parseInt(st.nextToken());
-                    newAbsoluteNum = Integer.parseInt(st.nextToken());
-                    while (station.absoluteNum != idx) {
-                        station = station.next;
-                    }
-                    rightStation = station.next;
-                    leftStation = station.prev;
+                    target = Integer.parseInt(st.nextToken());
+                    uniqueNumber = Integer.parseInt(st.nextToken());
 
-                    sb.append(rightStation.absoluteNum).append("\n");
-
-                    newStation = new Station(station, newAbsoluteNum, rightStation);
-                    rightStation.prev = newStation;
-                    station.next = newStation;
+                    sb.append(stationList.nextNode[target] + "\n");
+                    stationList.add(target, uniqueNumber);
 
                     break;
                 case "BP":
-                    idx = Integer.parseInt(st.nextToken());
-                    newAbsoluteNum = Integer.parseInt(st.nextToken());
+                    target = Integer.parseInt(st.nextToken());
+                    uniqueNumber = Integer.parseInt(st.nextToken());
 
-                    while (station.absoluteNum != idx) {
-                        station = station.next;
-                    }
-                    rightStation = station.next;
-                    leftStation = station.prev;
-
-                    sb.append(leftStation.absoluteNum).append("\n");
-
-                    newStation = new Station(leftStation, newAbsoluteNum, station);
-                    leftStation.next = newStation;
-                    station.prev = newStation;
+                    sb.append(stationList.prevNode[target] + "\n");
+                    stationList.add(stationList.prevNode[target], uniqueNumber);
 
                     break;
                 case "CN":
-                    idx = Integer.parseInt(st.nextToken());
-                    while (station.absoluteNum != idx) {
-                        station = station.next;
-                    }
+                    target = Integer.parseInt(st.nextToken());
 
-                    removeStation = station.next;
-                    sb.append(removeStation.absoluteNum).append("\n");
-
-                    rightStation = removeStation.next;
-                    leftStation = removeStation.prev;
-
-                    if (rightStation.equals(station)) {
-                        station.next = station;
-                        station.prev = station;
-                    } else {
-                        rightStation.prev = leftStation;
-                        leftStation.next = rightStation;
-                    }
+                    sb.append(stationList.nextNode[target] + "\n");
+                    stationList.delete(stationList.nextNode[target]);
 
                     break;
                 case "CP":
-                    idx = Integer.parseInt(st.nextToken());
-                    while (station.absoluteNum != idx) {
-                        station = station.next;
-                    }
-                    removeStation = station.prev;
+                    target = Integer.parseInt(st.nextToken());
 
-                    sb.append(removeStation.absoluteNum).append("\n");
+                    sb.append(stationList.prevNode[target] + "\n");
+                    stationList.delete(stationList.prevNode[target]);
 
-                    rightStation = removeStation.next;
-                    leftStation = removeStation.prev;
-
-                    if (leftStation.equals(station)) {
-                        station.next = station;
-                        station.prev = station;
-                    } else {
-                        rightStation.prev = leftStation;
-                        leftStation.next = rightStation;
-                    }
                     break;
 
                 default:
                     break;
             }
         }
-        sb.setLength(sb.length()-1);
+        sb.setLength(sb.length() - 1);
         System.out.println(sb.toString());
 
     }
