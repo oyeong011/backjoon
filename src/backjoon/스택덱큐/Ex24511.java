fb08d5487298147bb9721d133326a16b3016f13d
oyeong011 <oyeong011@gmail.com>
2023-11-01 11:04:16 +0900

    BackJoon : 24511 큐구조구현으로 성공

--- src/backjoon/스택덱큐/Ex24511.java
+++ src/backjoon/스택덱큐/Ex24511.java
@@ -3,11 +3,70 @@ package backjoon.스택덱큐;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Ex24511 {
 
+    static class Queue{
+        private Node head;
+        private Node tail;
+        private int size;
+        
+        private static class Node {
+            Node next;
+            Node prev;
+            int num;
 
+            Node(Node prev, int num, Node next){
+                this.prev = prev;
+                this.next = next;
+                this.num = num;
+            }
+        }
+        public void push(int val){
+            Node last = tail;
+            Node newNode = new Node(last, val, null);
+            tail = newNode;
+            size++;
+
+            if(last == null){
+                head = newNode;
+            } else {
+                last.next = newNode;
+            }
+        }
+
+        public void pushFirst(int val){
+            Node first = head;
+            Node newNode = new Node(null, val, head);
+            head = newNode;
+            size++;
+
+            if(first == null){
+                tail = newNode;
+            } else {
+                first.prev = newNode;
+            }
+        }
+        public int pop(){
+            if(head == null){
+                throw new RuntimeException();
+            }
+            int returnVal = head.num;
+            Node newNode = head.next;
+
+            head = newNode;
+            size--;
+
+            if(newNode == null){
+                tail = newNode;
+            } else {
+                newNode.prev = null;
+            }
+            return returnVal;
+        }
+    }
     public static int[] makeList(StringTokenizer st, int[] List){
         int i=0;
         while (st.hasMoreTokens()) {
@@ -16,20 +75,18 @@ public class Ex24511 {
         }
         return List;
     }
-    public static int[] inputDataArray(int input,int[] dataList, int[] dataStructure, StringBuilder sb, int N){
-        int arrow, tmp;
-        arrow = input;
-        for(int i = 0;i < N; i++){
-            if(dataStructure[i]==0){
-                tmp = dataList[i];
-                dataList[i] = arrow;
-                arrow = tmp;
-            } else {
-                continue;
+    public static Queue makeQueueList(StringTokenizer st, int[] dataStructure){
+        int i=0;
+        Queue queue = new Queue();
+        while (st.hasMoreTokens()) {
+            if(dataStructure[i] == 0){
+                queue.pushFirst(Integer.parseInt(st.nextToken()));
+            } else{
+                st.nextToken();
             }
+            i++;
         }
-        sb.append(arrow + " ");
-        return dataList;
+        return queue;
     }
     
     public static void main(String[] args) throws IOException{
@@ -41,13 +98,13 @@ public class Ex24511 {
         st = new StringTokenizer(br.readLine(), " ");
         dataStructure = makeList(st, dataStructure);
         
-        int[] dataList = new int[N];
+        
         st = new StringTokenizer(br.readLine(), " ");
-        dataList = makeList(st, dataList);
+        Queue queueList = makeQueueList(st, dataStructure);
+        
         
         st = new StringTokenizer(br.readLine(), " ");
         int inputDataCount = Integer.parseInt(st.nextToken());
-        
         int[] inputData = new int[inputDataCount];
         st = new StringTokenizer(br.readLine(), " ");
         
@@ -55,7 +112,8 @@ public class Ex24511 {
         
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < inputDataCount; i++){
-            dataList = inputDataArray(inputData[i], dataList, dataStructure, sb, N);
+            queueList.push(inputData[i]);
+            sb.append(queueList.pop()+ " ");
         }
         
         System.out.println(sb.toString());
